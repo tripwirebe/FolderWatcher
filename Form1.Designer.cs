0134872641cb5cecc8a9f1c8d6e37258758225c5
michel.brouckaert <michel.brouckaert@gmail.com>
2021-07-11 11:56:49 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace FolderWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fldrdSelect = new System.Windows.Forms.FolderBrowserDialog();
+            this.fsWatcher = new System.IO.FileSystemWatcher();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSourceSelect = new System.Windows.Forms.Button();
+            this.btnTarget = new System.Windows.Forms.Button();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtTarget = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fsWatcher)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fsWatcher
+            // 
+            this.fsWatcher.EnableRaisingEvents = true;
+            this.fsWatcher.SynchronizingObject = this;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Target";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(336, 71);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "&Start ";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSourceSelect
+            // 
+            this.btnSourceSelect.Location = new System.Drawing.Point(417, 13);
+            this.btnSourceSelect.Name = "btnSourceSelect";
+            this.btnSourceSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSourceSelect.TabIndex = 3;
+            this.btnSourceSelect.Text = "S&ource";
+            this.btnSourceSelect.UseVisualStyleBackColor = true;
+            this.btnSourceSelect.Click += new System.EventHandler(this.btnSourceSelect_Click);
+            // 
+            // btnTarget
+            // 
+            this.btnTarget.Location = new System.Drawing.Point(417, 38);
+            this.btnTarget.Name = "btnTarget";
+            this.btnTarget.Size = new System.Drawing.Size(75, 23);
+            this.btnTarget.TabIndex = 4;
+            this.btnTarget.Text = "&Target";
+            this.btnTarget.UseVisualStyleBackColor = true;
+            this.btnTarget.Click += new System.EventHandler(this.btnTarget_Click);
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(61, 13);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(350, 20);
+            this.txtSource.TabIndex = 5;
+            this.txtSource.Text = global::FolderWatcher.Properties.Settings.Default.SourcePath;
+            // 
+            // txtTarget
+            // 
+            this.txtTarget.Location = new System.Drawing.Point(61, 40);
+            this.txtTarget.Name = "txtTarget";
+            this.txtTarget.Size = new System.Drawing.Size(350, 20);
+            this.txtTarget.TabIndex = 6;
+            this.txtTarget.Text = global::FolderWatcher.Properties.Settings.Default.TargetPath;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(418, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Sto&p";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 106);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTarget);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.btnTarget);
+            this.Controls.Add(this.btnSourceSelect);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Folder watcher";
+            ((System.ComponentModel.ISupportInitialize)(this.fsWatcher)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog fldrdSelect;
+        private System.IO.FileSystemWatcher fsWatcher;
+        private System.Windows.Forms.TextBox txtTarget;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Button btnTarget;
+        private System.Windows.Forms.Button btnSourceSelect;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FolderWatcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSourceSelect_Click(object sender, EventArgs e)
+        {
+            fldrdSelect.ShowDialog();
+            Properties.Settings.Default.SourcePath = fldrdSelect.SelectedPath;
+            txtSource.Text = Properties.Settings.Default.SourcePath;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnTarget_Click(object sender, EventArgs e)
+        {
+            fldrdSelect.ShowDialog();
+            Properties.Settings.Default.TargetPath = fldrdSelect.SelectedPath;
+            txtTarget.Text = Properties.Settings.Default.TargetPath;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            fsWatcher.Path = Properties.Settings.Default.SourcePath;
+            fsWatcher.Created += FsWatcher_Created;
+            btnSave.Enabled = false;
+        }
+
+        private void FsWatcher_Created(object sender, System.IO.FileSystemEventArgs e)
+        {
+            WaitForFile(e.FullPath);
+            if (e.FullPath.EndsWith(".png") || e.FullPath.EndsWith(".jpg"))
+            {
+                try
+                {
+                    File.Copy(e.FullPath, string.Concat(Properties.Settings.Default.TargetPath, "\\", e.Name), true);
+                }
+                catch (IOException ex)
+                {
+                    throw;
+                }
+            }
+            File.Delete(e.FullPath);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fsWatcher.Created -= FsWatcher_Created;
+            button1.Enabled = false;
+        }
+        private void WaitForFile(string fullPath)
+        {
+            while (true)
+            {
+                try
+                {
+                    using (StreamReader stream = new StreamReader(fullPath))
+                    {
+                        break;
+                    }
+                }
+                catch
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
